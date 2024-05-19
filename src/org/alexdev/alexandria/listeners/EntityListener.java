12a202c8451c26245feb93787435f187aad5b6aa
Quackster <quacksterr.15@gmail.com>
2024-05-19 21:41:51 +1000

    Add way to get membranes

--- src/org/alexdev/alexandria/listeners/EntityListener.java
+++ src/org/alexdev/alexandria/listeners/EntityListener.java
@@ -1,12 +1,24 @@
 package org.alexdev.alexandria.listeners;
 
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
 
 public class EntityListener implements Listener {
+    private final Random random;
+
+    public EntityListener() {
+        this.random = new Random();
+    }
+
     @EventHandler
     public void onPlayerDamage(EntityDamageEvent event) {
         if (event.getEntityType() != EntityType.PLAYER) {
@@ -24,4 +36,34 @@ public class EntityListener implements Listener {
         }
     }
 
+    @EventHandler
+    public void onBatDeathEvent(EntityDeathEvent event) {
+        if (event.getEntityType() == EntityType.BAT) {
+            if (event.getEntity().getKiller() != null) {
+                Player killer = event.getEntity().getKiller();
+                ItemStack handItem = killer.getInventory().getItemInMainHand();
+
+                int maxMembraneChance = 1;
+
+                if (handItem.containsEnchantment(Enchantment.LOOT_BONUS_MOBS)) {
+                    int lootingLevel = handItem.getEnchantmentLevel(Enchantment.LOOT_BONUS_MOBS);
+
+                    switch (lootingLevel) {
+                        case 1:
+                            maxMembraneChance = 2;
+                            break;
+                        case 2:
+                            maxMembraneChance = 3;
+                            break;
+                        case 3:
+                            maxMembraneChance = 4;
+                            break;
+                    }
+                }
+
+                int membraneDropCount = random.nextInt(maxMembraneChance) + 1; // Randomly decide the drop count
+                event.getDrops().add(new ItemStack(Material.PHANTOM_MEMBRANE, membraneDropCount));
+            }
+        }
+    }
 }
